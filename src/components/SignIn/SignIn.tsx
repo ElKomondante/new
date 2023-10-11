ed74ff42fe6805b6b5899527f150a742967b2755
ElKomondante <124619203+ElKomondante@users.noreply.github.com>
2023-10-11 16:40:15 +0300

    Delete src directory

--- src/components/SignIn/SignIn.tsx
+++
@@ -1,9 +0,0 @@
-
-const Sign=(props:any) => {
-return(
-    <div style={{width:'300px',height:'100px', backgroundColor:'#ededed', margin:'0 auto', display:'flex'}}>
-        <h2 style={{fontSize:'77px', display:'flex', alignItems:'center', margin:'0 auto'}}  >Sign In</h2>
-    </div>
-)
-}
-export { Sign }
